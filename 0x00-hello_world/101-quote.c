6f85192b54c9dfeb1a63f605d1cfad941f4ac96e
Tobi <onipede89@gmail.com>
2022-09-09 03:24:31 +0000

    corrected

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ *  * main - Entry point
+ *   * Return: Always 1 (Success)
+ *    */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
