5ac468e5dc0246f6a33ade0dc3605d4c96ce02bb
Tobi <onipede89@gmail.com>
2022-09-09 02:23:17 +0000

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *main - entry point
+ *description - prints/outputs using the puts function
+ *Returns value 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
