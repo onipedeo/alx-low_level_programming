55be7cf3a2dca0ee5e1194d4e5d37df100537d8f
Tobi <onipede89@gmail.com>
2022-09-10 04:11:58 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, p;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	for (p = i + 1; p <= '9'; p++)
+	{
+	if (p != i)
+	{
+	putchar(i);
+	putchar(p);
+
+	if (i == '8' && p == '9')
+	continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+putchar('\n');
+
+return (0);
+}
