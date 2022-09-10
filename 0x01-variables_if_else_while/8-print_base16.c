0be9caba7c9619bafcea38d07d39fba7a0d86a4a
Tobi <onipede89@gmail.com>
2022-09-10 03:12:31 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char low;
+
+	for (i = '0'; i <= '9'; i++)
+	putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+return (0);
+}
