018a5c9db73b28a949045c74be8871c1abb4569d
Tobi <onipede89@gmail.com>
2022-09-14 03:55:16 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * description: prints all the alphabets in lowervase
+ * Return: Always (0) Success
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	_putchar('\n');
+}
