a10df7c0a9dbc717efdb2cdb495412248a588c81
Tobi <onipede89@gmail.com>
2022-10-14 02:18:27 +0000

    correction on variadic_function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,7 +14,8 @@ typedef struct print
 	void (*f)(va_list);
 	
 } print_t;
-int _putchar(char)
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator. const unsigned int n, ...);
 void print_strings(const char *separator. const unsigned int n, ...);
